90e554f9032c7fb00425bdfe7d021a997a9a687e
Jackson Booth <jacksontbooth@icloud.com>
2019-07-15 09:53:03 +1000

    fixed intergration with .h

---
+++ exmp1/invertedIndex.c
@@ -0,0 +1,21 @@
+#include "invertedIndex.h"
+#include <string.h>
+
+char * normaliseWord(char *str){
+    return("x\n");
+}
+
+InvertedIndexBST generateInvertedIndex(char *collectionFilename){
+    return(0);
+}
+
+void printInvertedIndex(InvertedIndexBST tree){
+}
+
+TfIdfList calculateTfIdf(InvertedIndexBST tree, char *searchWord , int D){
+    return(0);
+}
+
+TfIdfList retrieve(InvertedIndexBST tree, char* searchWords[] , int D){
+    return(0);
+}
