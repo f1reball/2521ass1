5ca5ef20d1551a0f29eb39944837c4cb8a4a187d
Jackson Booth <jacksontbooth@icloud.com>
2019-07-15 10:19:27 +1000

    added lowering to the function

--- exmp1/invertedIndex.c
+++ exmp1/invertedIndex.c
@@ -1,8 +1,31 @@
 #include "invertedIndex.h"
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 char * normaliseWord(char *str){
-    return("x\n");
+
+    //first get length of the string
+    //then we filter the string by removing each letter
+    //we want it fast so if we can scan the string once its then gonna be better
+
+
+//generates the length of the string
+    int length = strlen(str);
+    int i = 0;
+    printf("%d\n", length);
+
+
+//loops through string and replaces position with lower case
+    while(str[i]){
+        str[i] = tolower(str[i]);
+        i++;
+    }
+
+
+
+
+    return(str);
 }
 
 InvertedIndexBST generateInvertedIndex(char *collectionFilename){
