ee93c7cdc68b6c5f23ad43710d6f9f13847fd32e
Jackson Booth <jacksontbooth@icloud.com>
2019-07-15 12:10:55 +1000

    removed front spaces

--- exmp1/invertedIndex.c
+++ exmp1/invertedIndex.c
@@ -15,8 +15,24 @@ char * normaliseWord(char *str){
     int i = 0;
     printf("%d\n", length);
 
+//we need to strip the string of trailing spaces either side
+//we also need to have the same memory allocation
+//lucky for us something called memmove exists :D (YAY)
+
+//we just need to find the spaces before the word
+
+    int shift_counter = 0;
+    while(str[i] == ' '){
+        shift_counter++;
+        i++;
+    }
+
+    //moves string to the front of the memory address
+    memmove(str, str+shift_counter, length-shift_counter);
+    str[length-shift_counter] = 0;
 
 //loops through string and replaces position with lower case
+    i = 0;
     while(str[i]){
         str[i] = tolower(str[i]);
         i++;
